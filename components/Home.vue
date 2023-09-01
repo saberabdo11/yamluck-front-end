927bcd8eacad3f362c4ee95ce019b80cebeb3cef
saberabdo11 <=>
2023-09-01 11:28:05 +0100

    image home

--- components/Home.vue
+++ components/Home.vue
@@ -4,32 +4,13 @@
       <div
         class="d-flex align-items-center justify-content-between flex-wrap py-4"
       >
-        <div class="col-12 col-lg-6 ">
-          <!-- <img src="../assets/images/s1.png" alt="" class="img-fluid" /> -->
-          <ImageSlide></ImageSlide>
+        <div class="col-12 col-lg-6 text-center">
+          <img src="~/assets/images/bg.png" class="img-home" />
         </div>
 
-        <div class="col-12 col-lg-6 text-center mt-0">
-          <img src="~/assets/images/bg.png" class="img-home" />
-          <!-- <div class="ms-lg-5 p-lg-5">
-            <div :class="['ms-lg-5 p-lg-5', { 'text-end': locale == 'ar' }]">
-              <h5 class="home__text ms-lg-5" style="font-size: 13px">
-                {{ details[`title_${locale}`] }}
-              </h5>
-              <h1 class="fw-bold ms-lg-5">
-                {{ details[`sub_title_${locale}`] }}
-              </h1>
-              <h2 class="fw-bold ms-lg-5" style="font-size: 20px">
-                {{ details[`desc_${locale}`] }}
-              </h2>
-              <button
-                href="#menu"
-                class="btn main-btn mt-4 rounded-pill ms-lg-5"
-              >
-                GET IN TOUCH
-              </button>
-            </div>
-          </div> -->
+        <div class="col-12 col-lg-6">
+          <!-- <img src="../assets/images/s1.png" alt="" class="img-fluid" /> -->
+          <ImageSlide></ImageSlide>
         </div>
       </div>
     </div>
