143e19af869d00c02c536f7f7e740f987e06fc1f
ahmedhazma1001 <ah2279314@gmail.com>
2023-10-15 04:55:10 +0300

    doneAll

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,16 +7,20 @@
       absolute
       tile
       color="red accent-2"
-      v-if="$nuxt.isOffline"
-    >
+      v-if="$nuxt.isOffline">
       {{ $t("you-are-offline") }} !
     </v-snackbar>
-    <Nuxt />
+    <Nuxt class="fadeIn" v-if="isloaded" />
+    <center class="loaderHam" v-if="!isloaded">
+      <img src="../assets/images/newLogo.png">
+    </center>
+
   </div>
 </template>
 <script>
 export default {
   data: () => ({
+    isloaded: false,
     snackbar: true,
   }),
   head() {
@@ -26,5 +30,76 @@ export default {
       title: this.$t("yammluck"),
     };
   },
+  mounted() {
+    document.onreadystatechange = ()=>{
+      if(document.readyState == "complete")
+        setTimeout( () => {
+          this.isloaded = true;
+        },2000);
+    }
+  }
 };
 </script>
+<style>
+.loaderHam {
+  height: 100vh;
+  position: relative;
+  padding-top: 31%;
+  animation-name: hamzLoader;
+  animation-duration: 5s;
+}
+.fadeIn {
+  animation-name: fadeIn;
+  animation-duration: 0.5s;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  25%{
+    opacity: 0.25;
+  }
+  50%{
+    opacity: 0.50;
+  }
+  75%{
+    opacity: 0.75;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes hamzLoader {
+  10% {
+    transform: scale(0.8);
+  }
+  20%{
+    transform: scale(1.1);
+  }
+  30%{
+    transform: scale(0.8);
+  }
+  40%{
+    transform: scale(1.1);
+  }
+  50%{
+    transform: scale(0.8);
+  }
+  60%{
+    transform: scale(1.1);
+  }
+  70%{
+    transform: scale(0.8);
+  }
+  80%{
+    transform: scale(1.1);
+  }
+  90%{
+    transform: scale(0.8);
+  }
+  10%{
+    transform: scale(1.1);
+  }
+}
+</style>
